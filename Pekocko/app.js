2c59173a0271422b28e78280a588c0402d6e2584
Kris Long <corpkris@gmail.com>
2021-03-24 15:37:24 -0600

    app.js removed test messages, added login post

--- Pekocko/app.js
+++ Pekocko/app.js
@@ -3,22 +3,30 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('Request Received');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-// test method
-app.use((req, res, next) => {
-  res.json({ message: 'Your request was successful.' });
-  next();
+app.post('/api/auth/signup', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Signup Successful.',
+  });
 });
 
-app.use((req, res, next) => {
-  console.log('Response Sent Successfully.');
+app.post('/api/auth/login', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Signup Successful.',
+  });
 });
 
 module.exports = app;
