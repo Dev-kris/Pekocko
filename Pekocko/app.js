13b8f58dac8b67415eca6584104f1163ab8147c6
Kris Long <corpkris@gmail.com>
2021-03-23 20:06:51 -0600

    updated app.js with test middleware

--- Pekocko/app.js
+++ Pekocko/app.js
@@ -2,4 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('Request Received');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+// test method
+app.use((req, res, next) => {
+  res.json({ message: 'Your request was successful.' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Response Sent Successfully.');
+});
+
 module.exports = app;
