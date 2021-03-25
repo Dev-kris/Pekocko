68d7ab4d33e340f7cc528139502ae182094d0d55
Kris Long <corpkris@gmail.com>
2021-03-25 07:02:38 -0600

    added schema reference and sauces post

--- Pekocko/app.js
+++ Pekocko/app.js
@@ -5,6 +5,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+//problem here importing sauce model
+//const sauces = require('./models/sauces');
+const Sauces = require('./models/sauces');
+
 const app = express();
 
 mongoose
@@ -52,16 +56,31 @@ app.post('/api/auth/login', (req, res, next) => {
   });
 });
 //sauces route, needs sauce array
-app.use('/api/sauces', (req, res, next) => {
-  const sauce = [];
-});
+app.use('/api/sauces', (req, res, next) => {});
 
 //sauces route, post new sauce
+//
 app.post('/api/sauce', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: 'Login Successful.',
+  const sauces = new Sauces({
+    name: req.body.name,
+    manufacturer: req.body.manufacturer,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    mainPepper: req.body.mainPepper,
+    heat: req.body.heat,
   });
+  sauces
+    .save()
+    .then(() => {
+      res.status(201).json({
+        message: 'Sauce Saved Successfully.',
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
 });
 
 module.exports = app;
