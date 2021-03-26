07cc57faa0d5a27b3e6acbbcda6d2ac1c8264f54
Kris Long <corpkris@gmail.com>
2021-03-25 22:02:41 -0600

    fixed issue of font not being recognized

--- Pekocko/models/sauces.js
+++ Pekocko/models/sauces.js
@@ -1,20 +1,18 @@
 const mongoose = require('mongoose');
- 
-//schema to format sauces in mongoDB
-const saucesSchema = mongoose.Schema( {
-    userId​:  {type: String, required: true},
-    name​:  {type: String, required: true},
-    manufacturer​:  {type: String, required: true},
-    description​:   {type: String, required: true},
-    mainPepper​:   {type: String, required: true},
-    imageUrl​:   {type: String, required: true},
-    heat​:  {type: Number, required: true},
-    likes​:  {type: Number, required: true},
-    dislikes​:  {type: Number, required: true},
-    usersLiked​: ​{type: Array, required: true},
-    usersDisliked​: {type: Array, required: true},
+
+const sauceSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number },
+  dislikes: { type: Number },
+  usersLiked: { type: [String], required: true },
+  usersDisliked: { type: [String], required: true },
 });
 
+module.exports = mongoose.model('Sauce', sauceSchema);
 
-
-module.exports = mongoose.model('sauces', saucesSchema);
+//usersDisliked​
