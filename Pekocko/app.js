d1e8ea8ab7225c09ce80d6db8daa4baeabbeb435
Kris Long <corpkris@gmail.com>
2021-04-28 16:41:00 +0200

    added modify put route for modifying sauce

--- Pekocko/app.js
+++ Pekocko/app.js
@@ -84,6 +84,34 @@ app.get('/api/sauces/:id', (req, res, next) => {
     });
 });
 
+//modify sauce recipe
+app.put('/api/sauces/:id', (req, res, next) => {
+  const sauce = new Sauce({
+    _id: req.params.id,
+    name: req.body.name,
+    manufacturer: req.body.manufacturer,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    mainPepper: req.body.mainPepper,
+    heat: req.body.heat,
+    likes: 0,
+    dislikes: 0,
+    usersLiked: [],
+    usersDisliked: [],
+  });
+  Sauce.updateOne({ _id: req.params.id }, sauce)
+    .then(() => {
+      res.status(201).json({
+        message: 'Sauce updated successfully',
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+});
+
 //sauces route, post new sauce
 //
 app.post('/api/sauces', (req, res, next) => {
