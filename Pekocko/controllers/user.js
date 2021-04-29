deaaba4408bf5b117dd442ebb5d93fe6e647c6f4
Kris Long <corpkris@gmail.com>
2021-04-29 12:02:41 +0200

    added jwt and implemented token in ctrl

--- Pekocko/controllers/user.js
+++ Pekocko/controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+jwt = require('jsonwebtoken');
 const User = require('../models/users');
 
 // signup
@@ -39,9 +40,12 @@ exports.login = (req, res, next) => {
               error: new Error('Incorrect password'),
             });
           }
+          const token = jwt.sign({ userId: user._id }, 'RANDOM_TOKEN_SECRET', {
+            expiresIn: '24h',
+          });
           res.status(200).json({
             userId: user._id,
-            token: 'token',
+            token: token,
           });
         })
         .catch((error) => {
