02eb2d2568d444fdc419808eca6d460ceecbca33
Kris Long <corpkris@gmail.com>
2021-04-30 11:39:03 +0200

    added userId to schema and controller

--- Pekocko/controllers/sauce.js
+++ Pekocko/controllers/sauce.js
@@ -7,7 +7,6 @@ exports.createSauce = (req, res, next) => {
   const url = req.protocol + '://' + req.get('host');
   req.body.sauce = JSON.parse(req.body.sauce);
   const sauce = new Sauce({
-    userId: req.params.id,
     name: req.body.sauce.name,
     manufacturer: req.body.sauce.manufacturer,
     description: req.body.sauce.description,
@@ -18,6 +17,7 @@ exports.createSauce = (req, res, next) => {
     dislikes: 0,
     usersLiked: [],
     usersDisliked: [],
+    userId: req.body.sauce.userId,
   });
   sauce
     .save()
