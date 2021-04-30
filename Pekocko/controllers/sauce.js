2579f151df67e77b58e31fc605cec1f28078af64
Kris Long <corpkris@gmail.com>
2021-04-30 12:18:32 +0200

    fixed typo in delete sauce controller

--- Pekocko/controllers/sauce.js
+++ Pekocko/controllers/sauce.js
@@ -112,7 +112,7 @@ exports.modifySauce = (req, res, next) => {
 
 //Deletes a single sauce recipe
 exports.deleteSauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params._id }).then((sauce) => {
+  Sauce.findOne({ _id: req.params.id }).then((sauce) => {
     const filename = sauce.imageUrl.split('/images/')[1];
     fs.unlink('images/' + filename, () => {
       Sauce.deleteOne({ _id: req.params.id })
