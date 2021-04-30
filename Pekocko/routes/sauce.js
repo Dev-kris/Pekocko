7f4f8a27e5de783b2ac4f4c5ee70b650eee95bc2
Kris Long <corpkris@gmail.com>
2021-04-30 11:09:17 +0200

    added userId to sauceSchema

--- Pekocko/routes/sauce.js
+++ Pekocko/routes/sauce.js
@@ -6,11 +6,12 @@ const multer = require('../middleware/multer-config');
 
 const sauceCtrl = require('../controllers/sauce');
 
-//sauces routes, authentication > multer 
+//sauces routes, authentication > multer
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, sauceCtrl.likeDislike);
 
 module.exports = router;
