6b96cff48fee3a0f2db776633863662e98d06b74
Kris Long <corpkris@gmail.com>
2021-05-01 13:00:57 +0200

    added liking system and route

---
+++ Pekocko/controllers/likes.js
@@ -0,0 +1,71 @@
+const Sauce = require('../models/Sauce');
+
+exports.likesSystem = (req, res, next) => {
+  const like = req.body.like;
+  const userId = req.body.userId;
+
+  if (like === 0) {
+    Sauce.findOne({
+      _id: req.params.id,
+    })
+      .then((sauce) => {
+        if (sauce.usersLiked.includes(userId)) {
+          Sauce.updateOne(
+            { _id: req.params.id },
+            { $pull: { usersLiked: userId }, $inc: { likes: -1 } }
+          )
+            .then(() => res.status(200).json({ message: 'Like removed' }))
+            .catch((error) =>
+              res.status(400).json({
+                error: error,
+              })
+            );
+        }
+        if (sauce.usersDisliked.includes(userId)) {
+          Sauce.updateOne(
+            { _id: req.params.id },
+            { $pull: { usersDisliked: userId }, $inc: { dislikes: -1 } }
+          )
+            .then(() => res.status(200).json({ message: 'Dislike removed' }))
+            .catch((error) =>
+              res.status(400).json({
+                error: error,
+              })
+            );
+        }
+      })
+      .catch((error) =>
+        res.status(404).json({
+          error: error,
+        })
+      );
+  }
+
+  if (like > 0) {
+    Sauce.updateOne(
+      { _id: req.params.id },
+      //tried to use $addToSet here, change to $push
+      { $push: { usersLiked: userId }, $inc: { likes: +1 } }
+    )
+      .then(() =>
+        res.status(200).json({
+          message: 'Liked!',
+        })
+      )
+      .catch((error) =>
+        res.status(400).json({
+          error: error,
+        })
+      );
+  }
+  if (like < 0) {
+    Sauce.updateOne(
+      { _id: req.params.id },
+      { $push: { usersDisliked: userId }, $inc: { dislikes: +1 } }
+    )
+      .then(() => {
+        res.status(200).json({ message: 'Disliked' });
+      })
+      .catch((error) => res.status(400).json({ error }));
+  }
+};
--- Pekocko/routes/sauce.js
+++ Pekocko/routes/sauce.js
@@ -5,6 +5,7 @@ const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
 const sauceCtrl = require('../controllers/sauce');
+const likeCtrl = require('../controllers/likes');
 
 //sauces routes, authentication > multer
 router.post('/', auth, multer, sauceCtrl.createSauce);
@@ -12,5 +13,6 @@ router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, likeCtrl.likesSystem);
 
 module.exports = router;
