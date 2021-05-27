1f79f1fa2311fe8333a10b55d82a0dbdab6aeff2
Kris Long <corpkris@gmail.com>
2021-05-27 15:44:39 +0200

    added basic mongoose validation for sauces and user

--- Pekocko/models/Sauce.js
+++ Pekocko/models/Sauce.js
@@ -2,12 +2,32 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const sauceSchema = mongoose.Schema({
-  name: { type: String, required: true, unique: true, maxlength: 20 }, //front end is not setup to notify
-  manufacturer: { type: String, required: true },
-  description: { type: String, required: true },
-  mainPepper: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    maxlength: [20, 'Name is too long'],
+  }, //front end is not setup to notify
+  manufacturer: {
+    type: String,
+    required: true,
+    minlength: [3, 'Please enter a valid manufacturer name!'],
+    maxlength: 20,
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: [10, 'Please enter a description'],
+    maxlength: 100,
+  },
+  mainPepper: {
+    type: String,
+    required: true,
+    minlength: [4, 'Please provide the pepper name'],
+    maxlength: 20,
+  },
   imageUrl: { type: String, required: true },
-  heat: { type: Number, required: true },
+  heat: { type: Number, required: true, min: 1, max: 10 },
   likes: { type: Number, required: true },
   dislikes: { type: Number, required: true },
   usersLiked: { type: Array, required: true },
--- Pekocko/models/users.js
+++ Pekocko/models/users.js
@@ -3,7 +3,12 @@ const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, 'Password too short!'],
+    maxlength: 12,
+  },
 });
 
 userSchema.plugin(uniqueValidator);
