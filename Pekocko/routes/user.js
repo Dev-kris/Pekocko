97778449060c0ae15151ccfd4eaf165636d1d6ce
Kris Long <corpkris@gmail.com>
2021-05-27 15:38:23 +0200

    removed express-validator

--- Pekocko/routes/user.js
+++ Pekocko/routes/user.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const userCtrl = require('../controllers/user');
 
+//router.post('/signup', userCtrl.validate('user'), userCtrl.signup);
+
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 
